5fa7e0bfd651bcebcefc125380b7eaa183c8854b
izaitseva <zaitseva02@gmail.com>
2022-10-02 22:25:35 +0300

    init

---
+++ task-1/hw-1-1.js
@@ -0,0 +1,18 @@
+// Завдання 1
+
+const categoriesEl = document.querySelector('ul#categories');
+const itemsEl = categoriesEl.querySelectorAll('.item');
+
+console.log(`Number of categories: ${itemsEl.length}`);
+itemsEl.forEach((item)=> {
+  console.log(" ");
+
+  const h2El = item.querySelector('h2');
+  
+  console.log(`Category: ${h2El.innerText}`); 
+  
+  const liEl = item.querySelectorAll('li');
+  
+  // console.log(`Elements: ${liEl.length}`);
+  console.log(`Elements: liEl.children`);
+});
---
+++ task-2/hw-1-2.js
@@ -0,0 +1,19 @@
+// Завдання 2
+
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos", 
+  "Herbs",
+  "Condiments",
+];
+
+
+const items = document.createElement("li");
+items.classList.add('item');
+
+console.log(items.innerText);
+
+const liEl = item.querySelectorAll('li');
+console.log(liEl);
