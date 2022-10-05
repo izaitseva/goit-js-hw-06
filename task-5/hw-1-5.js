c32dad2cd491fb66afeb1f9d11c7e59d20b84dd7
v-adamenko <v.adamenko@polytech.software>
2022-10-06 00:56:08 +0300

    task-5, task-6

--- task-5/hw-1-5.js
+++ task-5/hw-1-5.js
@@ -1,15 +1,18 @@
-// input#name-input
 
 const refs = {
     inputName: document.querySelector("#name-input"),
     outputName: document.querySelector("#name-output"),
 };
 
-// ${inputName}
-
 refs.inputName.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-    console.log(event.currentTarget.value);
-    refs.outputName.textContent = event.currentTarget.value;
+    let value = event.currentTarget.value;
+    console.log(value);
+
+    if(value === "") {
+        refs.outputName.textContent = "Anonymous";
+    } else {
+        refs.outputName.textContent = value;
+    }
 }
--- task-6/hw-1-6.js
+++ task-6/hw-1-6.js
@@ -1,17 +1,23 @@
-
-
 const input = document.querySelector("#validation-input");
-let dataInputLength = document.querySelector(('[data-length="6"]'));
-const inputValue = " ";
-
-// ${inputName}
 
 input.addEventListener('blur', onInputBlur);
-    function onInputBlur() {
-    if(input == dataInputLength) {
-        return inputValue;
-    }
-    console.log('всё ок');
-}
 
-// input#name-input
+function onInputBlur(event) {
+    let inputEl = event.target;
+
+    let dataLength = inputEl.getAttribute("data-length");
+    let inputValue = inputEl.value;
+    let inputLength = inputValue.length;
+
+
+
+    if (inputLength === Number(dataLength)) {
+        inputEl.classList.add("valid");
+        inputEl.classList.remove("invalid");
+    } else {
+        inputEl.classList.add("invalid");
+        inputEl.classList.remove("valid");
+    }
+
+    console.log(inputEl);
+}
