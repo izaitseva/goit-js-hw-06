37927b2674a391df15ec137f30ca235141533b20
Iryna Zaitseva <zaitseva02@gmail.com>
2022-10-05 17:34:11 +0300

    task-4

--- task-4/hw-1-4.js
+++ task-4/hw-1-4.js
@@ -1,2 +1,16 @@
-const counterEl = document.querySelector('ul#counter');
+const decrButton = document.querySelector('[data-action="decrement"]');
+const incrButton = document.querySelector('[data-action="increment"]');
+
 let counterValue = 0;
+
+decrButton.addEventListener('click', () => {
+    counterValue = counterValue - 1;
+    let decrCounterEl = document.querySelector("#value");
+    decrCounterEl.textContent = counterValue;
+});
+
+incrButton.addEventListener('click', () => {
+    counterValue = counterValue + 1;
+    let decrCounterEl = document.querySelector("#value");
+    decrCounterEl.textContent = counterValue;
+});
