aed28369c87737ab89677fe6eff782c10de96fcb
v-adamenko <v.adamenko@polytech.software>
2022-10-05 23:49:17 +0300

    task-5

--- task-5/hw-1-5.js
+++ task-5/hw-1-5.js
@@ -1,10 +1,15 @@
 // input#name-input
 
-const refs {
+const refs = {
     inputName: document.querySelector("#name-input"),
     outputName: document.querySelector("#name-output"),
 };
 
 // ${inputName}
 
-refs.inputName  
+refs.inputName.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    console.log(event.currentTarget.value);
+    refs.outputName.textContent = event.currentTarget.value;
+}
